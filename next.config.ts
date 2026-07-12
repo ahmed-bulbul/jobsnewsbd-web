7d757732787712b75ba17cebbebb8b6e8cee6646
ahmed-bulbul <ahmedbulbul.cse@gmail.com>
2026-07-12 17:37:08 +0600

    Added commenthh

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'http', hostname: 'localhost', port: '8081', pathname: '/uploads/**' },
+      { protocol: 'https', hostname: 'http://api.jobradarbd.com/', pathname: '/uploads/**' },
     ],
   },
 };
