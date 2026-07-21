e9f02eb03437ad22d8978f1f4e7ef379b46307e6
ahmed-bulbul <ahmedbulbul.cse@gmail.com>
2026-07-21 22:00:47 +0600

    Update font

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // { protocol: 'http', hostname: 'localhost', port: '8081', pathname: '/uploads/**' },
+      { protocol: 'http', hostname: 'localhost', port: '8081', pathname: '/uploads/**' },
       { protocol: 'https', hostname: 'http://api.jobradarbd.com/', pathname: '/uploads/**' },
     ],
   },
